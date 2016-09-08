81e35a41021f640182f24cc1fcbc37a7f6d51598
yberry <y_berry@hotmail.fr>
2016-09-08 17:44:27 +0200

    Serial

--- Assets/Bombe/Bombe.cs
+++ Assets/Bombe/Bombe.cs
@@ -1,15 +1,69 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class Bombe : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    #region Bombe
+    private static Bombe instance;
+    public static Bombe Get
+    {
+        get
+        {
+            return instance;
+        }
+    }
+    #endregion
+
+    #region NumSerie
+    private string numSerie;
+    public bool numPair
+    {
+        get
+        {
+            return numSerie[5] % 2 == 0;
+        }
+    }
+    public bool voyelle
+    {
+        get
+        {
+            foreach (char c in numSerie)
+            {
+                if ("AEIOUY".IndexOf(c) >= 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+	    if (instance == null)
+        {
+            instance = this;
+        }
+        SetSerial();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void SetSerial()
+    {
+        numSerie = "";
+        for (int i = 0; i < 6; i++)
+        {
+            numSerie += RandomLetter(i == 5);
+        }
+    }
+
+    char RandomLetter(bool last)
+    {
+        string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return alpha[Random.Range(last ? 26 : 0, alpha.Length)];
+    }
 }
