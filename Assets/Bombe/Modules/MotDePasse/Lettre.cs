af308fca96527fc220a65bf334096778f7142c61
yberry <y_berry@hotmail.fr>
2016-09-27 10:52:10 +0200

    MDP 1

---
+++ Assets/Bombe/Modules/MotDePasse/Lettre.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class Lettre : MonoBehaviour
+{
+    private char[] lettres;
+    private int current = 0;
+    public char currentLettre
+    {
+        get
+        {
+            return lettres[current];
+        }
+    }
+
+    private Text affichage;
+
+    public Button flecheHaut;
+    public Button flecheBas;
+
+    void Start()
+    {
+        affichage = GetComponent<Text>();
+        Affiche();
+        flecheHaut.onClick.AddListener(Haut);
+        flecheBas.onClick.AddListener(Bas);
+    }
+
+    void Affiche()
+    {
+        affichage.text = currentLettre.ToString();
+    }
+
+    void Haut()
+    {
+        current--;
+        if (current < 0)
+        {
+            current = lettres.Length - 1;
+        }
+        Affiche();
+    }
+
+    void Bas()
+    {
+        current++;
+        if (current >= lettres.Length)
+        {
+            current = 0;
+        }
+        Affiche();
+    }
+}
--- Assets/Bombe/Modules/MotDePasse/MotDePasse.cs
+++ Assets/Bombe/Modules/MotDePasse/MotDePasse.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class MotDePasse : Module {
 
+    public Lettre[] lettres;
+
 	// Use this for initialization
 	void Start () {
 	
