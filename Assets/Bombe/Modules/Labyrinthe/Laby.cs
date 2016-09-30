a41d27176d5e49d3ad37f5bedc5c3d29f54f82cc
yberry <y_berry@hotmail.fr>
2016-09-30 10:49:38 +0200

    Laby 1

---
+++ Assets/Bombe/Modules/Labyrinthe/Laby.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Laby
+{
+    struct Place
+    {
+        public bool murHaut;
+        public bool murGauche;
+        public bool murBas;
+        public bool murDroite;
+        public Place(bool haut, bool gauche, bool bas, bool droite)
+        {
+            murHaut = haut;
+            murGauche = gauche;
+            murBas = bas;
+            murDroite = droite;
+        }
+    }
+
+    private Place[,] places;
+
+    public Laby()
+    {
+
+    }
+
+    private static readonly Laby[] labys;
+
+    public static Laby RandomLaby()
+    {
+        return labys[Random.Range(0, labys.Length)];
+    }
+}
--- Assets/Bombe/Modules/Labyrinthe/Labyrinthe.cs
+++ Assets/Bombe/Modules/Labyrinthe/Labyrinthe.cs
@@ -1,15 +1,61 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Labyrinthe : Module {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private const int nbColonnes = 6;
+    private const int nbLignes = 6;
+
+    private Vector2 triangle;
+    private Vector2 carre;
+
+    private Laby laby;
+
+    public Button flecheHaut;
+    public Button flecheGauche;
+    public Button flecheBas;
+    public Button flecheDroite;
+
+    // Use this for initialization
+    void Start () {
+
+        laby = Laby.RandomLaby();
+
+        int tx = Random.Range(0, nbColonnes);
+        int ty = Random.Range(0, nbLignes);
+        triangle = new Vector2(tx, ty);
+
+        int cx = Random.Range(0, nbColonnes);
+        int cy = Random.Range(0, nbLignes);
+        while (cx == tx && ty == cy)
+        {
+            cx = Random.Range(0, nbColonnes);
+            cy = Random.Range(0, nbLignes);
+        }
+        carre = new Vector2(tx, ty);
+
+
+    }
+
+    void Haut()
+    {
+
+    }
+
+    void Gauche()
+    {
+
+    }
+
+    void Bas()
+    {
+
+    }
+
+    void Droite()
+    {
+
+    }
+
 }
