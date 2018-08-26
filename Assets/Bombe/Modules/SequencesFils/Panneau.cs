dafe1c4f67f8fe80b35598686cd14b56614f3d93
Yanis <y_berry@hotmail.fr>
2018-08-27 00:38:29 +0200

    Start sequences fils

--- Assets/Bombe/Modules/SequencesFils/Panneau.cs
+++ Assets/Bombe/Modules/SequencesFils/Panneau.cs
@@ -1,7 +1,32 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Panneau : MonoBehaviour {
 
+    private SequencesFils sequence;
 
+    public Text[] labelsFils;
+
+    public bool Complete
+    {
+        get
+        {
+            return true;
+        }
+    }
+
+    public void SetModule(SequencesFils seq)
+    {
+        sequence = seq;
+    }
+
+    public void SetLabels(int nb)
+    {
+        int start = 3 * nb + 1;
+        for (int i = 0; i < 3; i++)
+        {
+            labelsFils[i].text = (start + i).ToString();
+        }
+    }
 }
--- Assets/Bombe/Modules/SequencesFils/SequencesFils.cs
+++ Assets/Bombe/Modules/SequencesFils/SequencesFils.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections.Generic;
 
 public class SequencesFils : Module {
@@ -9,4 +10,72 @@ public class SequencesFils : Module {
         { Color.blue, new List<string>() { "B", "AC", "B", "A", "B", "BC", "C", "AC", "A" } },
         { Color.black, new List<string>() { "ABC", "AC", "B", "AC", "B", "BC", "AB", "C", "C" } }
     };
+
+    private const int nbPanneaux = 4;
+    private int currentLayer = 0;
+    private Panneau[] panneaux;
+
+    public Button boutonHaut;
+    public Button boutonBas;
+    public Light[] succes;
+    public Panneau prefabPanneau;
+
+    private void Start()
+    {
+        foreach (Light light in succes)
+        {
+            light.color = Color.green;
+            light.enabled = false;
+        }
+
+        boutonHaut.onClick.AddListener(Haut);
+        boutonBas.onClick.AddListener(Bas);
+
+        panneaux = new Panneau[nbPanneaux];
+        for (int i = 0; i < nbPanneaux; i++)
+        {
+            panneaux[i] = Instantiate(prefabPanneau);
+            panneaux[i].SetModule(this);
+        }
+    }
+
+    void Restart()
+    {
+
+    }
+
+    void Haut()
+    {
+        if (!Desamorce && currentLayer > 0)
+        {
+            panneaux[currentLayer].gameObject.SetActive(false);
+            panneaux[--currentLayer].gameObject.SetActive(true);
+        }
+    }
+
+    void Bas()
+    {
+        if (Desamorce)
+        {
+            return;
+        }
+
+        if (panneaux[currentLayer].Complete)
+        {
+            succes[currentLayer].enabled = true;
+            panneaux[currentLayer].gameObject.SetActive(false);
+            if (++currentLayer == nbPanneaux)
+            {
+                Resolu();
+            }
+            else
+            {
+                panneaux[currentLayer].gameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            Faute();
+        }
+    }
 }
